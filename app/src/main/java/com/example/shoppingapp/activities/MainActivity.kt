012e7f3eae0c26d70542d5ea34e6bed600cc7022
Vinod <vinodnyamagoudar@gmail.com>
2024-03-08 17:55:00 +0530

    initial commit

--- app/src/main/java/com/example/shoppingapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/activities/MainActivity.kt
@@ -1,48 +1,38 @@
 package com.example.shoppingapp.activities
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppingapp.R
 import com.example.shoppingapp.adapter.CategoryAdapter
 import com.example.shoppingapp.model.Category
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import org.json.JSONObject
-import java.io.IOException
+import com.example.shoppingapp.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Parse JSON data and set up RecyclerView
-        val categories = readJsonData(this)
-        setupRecyclerView(categories)
-    }
+        // Observe data from ViewModel
+        viewModel.categories.observe(this) { categories ->
+            setupRecyclerView(categories)
+        }
 
+        // Fetch data from JSON file
+        viewModel.fetchData(this)
+
+        //favourite list
+
+    }
     private fun setupRecyclerView(categories: List<Category>) {
         val outerRecyclerView: RecyclerView = findViewById(R.id.category_recyclerview)
         outerRecyclerView.layoutManager = LinearLayoutManager(this)
         outerRecyclerView.adapter = CategoryAdapter(categories)
-    }
-
-    fun readJsonData(context: Context): List<Category> {
-        try {
-            val jsonFile = context.assets.open("data.json")
-            val jsonString = jsonFile.bufferedReader().use { it.readText() }
-            val gson = Gson()
-            val jsonObject = JSONObject(jsonString)
-            val categoriesArray = jsonObject.getJSONArray("categories")
-            val categoriesType = object : TypeToken<List<Category>>() {}.type
-            return gson.fromJson(categoriesArray.toString(), categoriesType)
-        } catch (e: IOException) {
-            e.printStackTrace()
-            return emptyList()
-        }
     }
 }
